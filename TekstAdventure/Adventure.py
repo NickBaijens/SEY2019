f0f071e507c8b8e6fb97c1c3262f99e82695bd1f
NickBaijens <nbnickbaijens@gmail.com>
2019-09-25 15:56:53 +0200

    simplified room initialization

--- TekstAdventure/Adventure.py
+++ TekstAdventure/Adventure.py
@@ -1,11 +1,9 @@
 from room import Room
 from key import Key
 from door import Door
+
 class Adventure:
     def __init__(self):
-        doors = []
-        keys = []
-        doors.append(Door(123))
-        keys.append(Key(123))
-        keys.append(Key(321))
-        firstRoom = Room("keuken","dit is een keuken", doors, keys)
+        firstRoom = Room("keuken","dit is een keuken", [Door(123)], [Key(123),Key(321)])
+        secondRoom = Room("woonkamer","dit is een woonkamer", [Door(123)], [])
+

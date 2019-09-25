6a0ba4d8de4c725d22cb88d92bb66cec1a51874c
NickBaijens <nbnickbaijens@gmail.com>
2019-09-25 13:11:19 +0200

    add classes room, key and door.py

---
+++ TekstAdventure/room.py
@@ -0,0 +1,6 @@
+class Room:
+    def __init__(self, name, description, doors, keys):
+        self.name = name
+        self.description = description
+        self.doors = doors
+        self.keys = keys

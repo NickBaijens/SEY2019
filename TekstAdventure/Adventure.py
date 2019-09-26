dce5401d3fb72026f61970ffeec592a34c00dd1f
NickBaijens <nbnickbaijens@gmail.com>
2019-09-26 15:56:24 +0200

    busy changing input

--- TekstAdventure/Adventure.py
+++ TekstAdventure/Adventure.py
@@ -2,21 +2,41 @@ from room import Room
 from key import Key
 from door import Door
 
-rooms = []
-currentRoom = 0
+class Adventure:
 
-firstRoom = Room(1,"een keuken", [Door(123, 1)], [Key(123),Key(321)])
-secondRoom = Room(2,"een woonkamer", [Door(123, 0)], [])
-rooms.append(firstRoom)
-rooms.append(secondRoom)
+    def __init__(self):
+        self.rooms = []
+        self.currentRoom = 0
+        self.inventory = []
 
-doorToOpen = 0
-keyToUse = 1
+        firstRoom = Room(1,"Je staat in de keuken. Er zijn 2 sleutels, een met het label '0', en een met het label '1'. Er is maar 1 deur.", [Door(123, 1)], [Key(123),Key(321)])
+        secondRoom = Room(2,"Je staat in de woonkamer, er is een deur", [Door(123, 0)], [])
+        self.rooms.append(firstRoom)
+        self.rooms.append(secondRoom)
+        "huidige kamer is: " + self.rooms[self.currentRoom].description
 
-if rooms[currentRoom].keys[keyToUse].openDoor(rooms[currentRoom].doors[doorToOpen]):
-    currentRoom = rooms[currentRoom].doors[doorToOpen].leadsTo
-    result = "deur opent."
-else:
-    result = "deur opent niet."
-print(result + " huidige kamer is: " + rooms[currentRoom].description)
+
+    def pickupKey(self, _key):
+
+        self.inventory = self.rooms[self.currentRoom].keys.pop(_key)
+
+
+    def openDoor(self, _door, _key):
+
+        if self.rooms[self.currentRoom].keys[_key].keyCode == self.rooms[self.currentRoom].doors[_door].unlockCode:
+            self.currentRoom = self.rooms[self.currentRoom].doors[_door].leadsTo
+            result = "deur opent."
+
+        else:
+            result = "deur opent niet."
+
+        return (result + " huidige kamer is: " + self.rooms[self.currentRoom].description)
+
+
+theGame = Adventure()
+
+
+while True:
+    keyToUse = int(input( "Welke wil je gebruiken?"))
+    print(theGame.openDoor(0,keyToUse))
 
